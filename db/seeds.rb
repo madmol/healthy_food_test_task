48e805eea4b02eab59ab69ffbfa8027462b1f9da
Dmitry O. Markov <markov.dm@inbox.ru>
2023-10-04 14:53:11 +0300

    add start menu.xml and seed it file to DB via seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'yaml'
+
+menu_file_path = Rails.root.join('db', 'menu.yml')
+
+if File.exist?(menu_file_path)
+  begin
+    data = YAML.load_file(menu_file_path)
+
+    data['ingredients'].each do |ingredient|
+      new_ingredient = Ingredient.new(name: ingredient)
+      unless new_ingredient.save
+        puts "Failed to save Ingredient '#{ingredient}': #{new_ingredient.errors.full_messages.join(', ')}"
+      end
+    end
+
+    data['dishes'].each do |dish_data|
+      dish = Dish.new(name: dish_data['name'])
+      missing_ingredients = []
+      dish_data['ingredients'].each do |ingredient_name|
+        ingredient = Ingredient.find_by(name: ingredient_name)
+        if ingredient
+          dish.ingredients << ingredient
+        else
+          missing_ingredients << ingredient_name
+        end
+      end
+
+      if missing_ingredients.empty?
+        unless dish.save
+          puts "Failed to save Dish '#{dish.name}': #{dish.errors.full_messages.join(', ')}"
+        end
+      else
+        puts "Failed to save dish '#{dish.name}' is missing the following ingredients: #{missing_ingredients.join(', ')}." \
+          " You should create it first."
+      end
+    end
+  rescue StandardError => e
+    puts "Error during seed data creation: #{e.message}"
+  end
+else
+  puts "menu.yml file not found in #{menu_file_path}. Please make sure the file exists."
+end
