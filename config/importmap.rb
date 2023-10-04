55ccd7f973763c7c764836899cf70ae14c61761b
Dmitry O. Markov <markov.dm@inbox.ru>
2023-10-04 21:37:36 +0300

    configure app to use bootstrap 5

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js" # use unpkg.com as ga.jspm.io contains a broken popper package
