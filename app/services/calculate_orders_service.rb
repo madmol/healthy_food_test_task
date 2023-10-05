f5b3cee896ba8a8b1afdf465c5d5842d7a49e3c5
Dmitry O. Markov <markov.dm@inbox.ru>
2023-10-05 19:16:07 +0300

    add find_dishes app/services/calculate_orders_service.rb

--- app/services/calculate_orders_service.rb
+++ app/services/calculate_orders_service.rb
@@ -1,11 +1,24 @@
 class CalculateOrdersService < ApplicationService
-  def initialize(orders)
-    @orders = orders
+  def initialize(data)
+    @allergen_ingredients_arry, @class = data
   end
 
-  def self.call(orders)
-    orders
+  def call
+    generate_hash
   end
 
   private
+
+  def find_dishes
+    @allergen_ingredients_arry.map do |allergen_ingredients|
+      @class.find_dishes_without_allergic_ingredients(allergen_ingredients.pluck(:id)).first&.name
+    end.compact
+  end
+
+  def generate_hash
+    find_dishes.tally
+      .sort_by(&:last)
+      .reverse_each
+      .map { |k, v| { name: k, count: v } }
+  end
 end
